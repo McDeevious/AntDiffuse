12cbb3628e7a481953abd05cdd04f9c40a69a33e
McDeevious <blainekmcdonald@gmail.com>
2025-10-24 16:39:18 +1300

    Tiles added

---
+++ Assets/Scripts/CameraFollowRooms.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class CameraFollowRooms : MonoBehaviour
+{
+    public Transform targetPosition;
+    public float moveSpeed = 5.0f;
+
+    private bool isMoving = false;
+
+    void Update()
+    {
+        if (isMoving && targetPosition != null)
+        {
+            transform.position = Vector3.Lerp(transform.position, targetPosition.position, Time.deltaTime * moveSpeed);
+        }
+
+        // clamp value if it is close enough to target
+        if (Vector3.Distance(transform.position, targetPosition.position) < 0.05f) 
+        {
+            transform.position = targetPosition.position;
+            isMoving = false;
+        }
+    }
+
+    public void MoveToRoom(Transform newTarget)
+    {
+        targetPosition = newTarget;
+        isMoving = true;
+    }
+}
