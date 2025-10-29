1c7ef663645322a08268c780975ae3e9e5a104ad
McDeevious <blainekmcdonald@gmail.com>
2025-10-29 21:33:17 +1300

    updated

---
+++ Assets/Scripts/KeypadScript.cs
@@ -0,0 +1,20 @@
+using TMPro;
+using UnityEngine;
+
+public class KeypadScript : MonoBehaviour
+{
+    public TMP_InputField charHolder;
+    public GameObject button1;
+    public GameObject button2;
+    public GameObject button3;
+    public GameObject button4;
+    public GameObject button5;
+    public GameObject button6;
+    public GameObject button7;
+    public GameObject button8;
+    public GameObject button9;
+    public GameObject clearButton;
+    public GameObject enterButton;
+
+
+}
